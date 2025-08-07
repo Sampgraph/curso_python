a50f7e7234244bec972bcb144fd120c9c75c36db
Wellington <wtpereira@gmail.com>
2025-08-06 21:59:09 -0300

    Criar o arquivo da Aula 02.

---
+++ Aulas/aula02_01.py
@@ -0,0 +1,17 @@
+# Diferentes formas de definir strings em Python
+# Usando aspas simples, aspas duplas, aspas triplas simples e aspas triplas duplas
+
+a = 'O autor disse assim: "A vida é bela!" e depois encerrou a palestra.'
+print(a)
+
+b = "O autor disse assim: 'A vida é bela!' e depois encerrou a palestra."
+print(b)
+
+c = '''O autor disse assim: "A vida é bela!" e depois encerrou a palestra.'''
+print(c)
+
+d = """O autor disse assim: 'A vida é bela!' e depois encerrou a palestra."""
+print(d)
+
+f = '''O autor disse assim: """A vida é bela!""" e depois encerrou a palestra.'''
+print(f)
